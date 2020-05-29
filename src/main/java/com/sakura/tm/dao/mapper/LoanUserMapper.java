966dff7b6fd00af1151cbd2bfe71025b95c852c0
RoadHLY <38309810+RoadHLY@users.noreply.github.com>
2020-05-29 11:18:10 +0800

    修改了代码

---
+++ src/main/java/com/sakura/tm/dao/mapper/LoanUserMapper.java
@@ -0,0 +1,9 @@
+package com.sakura.tm.dao.mapper;
+
+import com.sakura.tm.common.entity.LoanUser;
+import com.sakura.tm.dao.generator.BaseGeneratorMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface LoanUserMapper extends BaseGeneratorMapper<LoanUser> {
+}
